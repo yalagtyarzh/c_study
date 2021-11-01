ec3db53c92d792c3720d0628cca1c6a37d9bff34
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-11-01 13:39:06 +0300

    feat(16, 17): Added 16 and 17 practical works

---
+++ 16/16.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void cleanConsole() {
+    system("cls");
+}
+
+void changeConsoleBackground() {
+    system("color 57");
+}
+
+void changeConsoleText() {
+    system("color 06");
+}
+
+void printSizeOfInt() {
+    cout << "[+]Размер типа данных int - " << sizeof(int) << endl;
+}
+
+
+void printSizeOfChar() {
+    cout << "[+]Размер типа данных char - " << sizeof(char) << endl;
+}
+
+void printSizeOfDouble() {
+    cout << "[+]Размер типа данных double - " << sizeof(double) << endl;
+}
+
+void printSizeOfFloat() {
+    cout << "[+]Размер типа данных float - " << sizeof(float) << endl;
+}
+
+void printSizeOfBool() {
+    cout << "[+]Размер типа данных bool - " << sizeof(bool) << endl;
+}
+
+void printHello() {
+    cout << "Привет я функция" << endl;
+}
+
+void printLogo() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << "# ";
+        }
+        cout << "\n";
+    }
+}
+
+
+
+int main()
+{
+    setlocale(0, "");
+    cleanConsole();
+    changeConsoleBackground();
+    changeConsoleText();
+    printSizeOfInt();
+    printSizeOfChar();
+    printSizeOfDouble();
+    printSizeOfFloat();
+    printSizeOfBool();
+    printLogo();
+    return 0;
+}
+
---
+++ 17/17.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float calc(float a, float b, char operation) {
+    if (b == 0 && operation == '/') {
+        return 0;
+    }
+
+    switch (operation) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '/': return a / b;
+    case '*': return a * b;
+    case '%': return int(a) % int(b);
+    case 'p': return pow(a, 2) * pow(b, 2);
+    case 'f': return 2019;
+    case '^': return  pow(a, b);
+    default:
+        cout << "Вы ввели некорректное значение. Вывод по умолчанию: ";
+        return 0;
+    }
+
+}
+
+int main()
+{
+    setlocale(0, "");
+
+    cout << "Введите первое число ";
+    float a;
+    cin >> a;
+    cout << "Введите второе число ";
+    float b;
+    cin >> b;
+    cout << "Введите операцию ";
+    char operation;
+    cin >> operation;
+
+    int result = calc(a, b, operation);
+
+    cout << result;
+
+    return 0;
+}
+
